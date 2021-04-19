3ffba4018ec3d4e2e7cc38b8cb8d1a71d4094b95
Ani Balasubramaniam <ani@anionline.me>
2021-04-19 11:54:33 -0700

    Bring back pi-gen based Wi-Fi env as step 1/2

--- bootstrap/01-run.sh
+++ bootstrap/01-run.sh
@@ -10,7 +10,7 @@ then
     echo "Skipping generation of wpa_supplicant.conf, one or more required Wi-Fi fields were not provided"
 else
     # Generate wpa_supplicant.conf in /boot so pi will copy it and disable rfkill on startup
-    echo "Generating wpa_supplicant.conf..."
+    echo "Generating wpa_supplicant.conf, step 2/2..."
     echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev" > ${BOOTFS_DIR}/wpa_supplicant.conf
     echo "update_config=1" >> ${BOOTFS_DIR}/wpa_supplicant.conf
     echo "country=${WPA_COUNTRY}" >> ${BOOTFS_DIR}/wpa_supplicant.conf
